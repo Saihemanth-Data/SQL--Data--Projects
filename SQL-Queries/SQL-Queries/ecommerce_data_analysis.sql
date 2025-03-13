7ac0abfbbd5b225e052ae2c4921238fb463ad0b1
saihemanth Bathula <saihemanth.bathula@gmail.com>
2025-03-13 17:32:50 -0500

    Update ecommerce_data_analysis.sql

--- SQL-Queries/SQL-Queries/ecommerce_data_analysis.sql
+++ SQL-Queries/SQL-Queries/ecommerce_data_analysis.sql
@@ -1,1 +1,11 @@
+-- E-Commerce Data Analysis
+-- This query analyzes product sales performance in an online store
 
+SELECT 
+    product_category,
+    product_name,
+    SUM(quantity_sold) AS total_units_sold,
+    SUM(revenue) AS total_revenue
+FROM ecommerce_sales
+GROUP BY product_category, product_name
+ORDER BY total_revenue DESC;
