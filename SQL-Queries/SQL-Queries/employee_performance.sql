43ee36c68ada9ee7b7756340ede26498853e1a7a
saihemanth Bathula <saihemanth.bathula@gmail.com>
2025-03-13 17:32:16 -0500

    Update employee_performance.sql

--- SQL-Queries/SQL-Queries/employee_performance.sql
+++ SQL-Queries/SQL-Queries/employee_performance.sql
@@ -1,1 +1,11 @@
+-- Employee Performance Analysis
+-- This query provides insights into employee performance based on completed tasks
 
+SELECT 
+    department,
+    employee_name,
+    COUNT(task_completed) AS total_tasks,
+    AVG(task_completion_time) AS avg_completion_time
+FROM employee_performance
+GROUP BY department, employee_name
+ORDER BY total_tasks DESC;
